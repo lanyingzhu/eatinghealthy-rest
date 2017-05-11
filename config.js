4c77168b30dd7b90558a86c29fafae3f0aa73008
Lanying <lanying.zhu.01@gmail.com>
2017-05-11 10:05:15 -0700

    Add all files

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/EatingHealthy',
+    'facebook': {
+        clientID: '1475635819117541',
+        clientSecret: 'bba826efd4f3330488d7007cf257b41a',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
---
+++ db.js
@@ -0,0 +1,107 @@
+[
+        {
+         "name":"Breakfast Grilled Cheese Tocas",
+         "area": "America",
+         "image":"images/Breakfast Grilled Cheese Tocas.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Asian-chicken-corn-and-noodle-soup",
+         "area": "Asianfood",
+         "image":"images/asian-chicken-corn-and-noodle-soup.jpeg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"'",
+         "comments": []
+      },
+      {
+         "name":"Pesto salmon and veggies-in-foil",
+         "area": "America",
+         "image":"images/pesto-salmon-and-italian-veggies-in-foil-srgb.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Fruit Bubble Tea",
+         "area": "Asianfood",
+         "image":"images/Fruit bubble tea.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Rich Chicken Stew",
+         "area": "Europe",
+         "image":"images/Rich Chicken Stew.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"KoreanBeef",
+         "area": "Asianfood",
+         "image":"images/KoreanBeef.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Shrimp-Pho-Soup",
+         "area": "Asianfood",
+         "image":"images/Easy-Pho-Recipe-a-homemade-shrimp-pho-soup.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Barbecue-pork-steamed-buns",
+         "area": "Asianfood",
+         "image":"images/barbecue-pork-steamed-buns.jpeg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Avocado-shrimp-zoodle",
+         "area": "America",
+         "image":"images/Avocado-shrimp-zoodle.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Baked-Mustard-Crusted-Salmon-Asparagus-Tarragon",
+         "area": "America",
+         "image":"images/baked-mustard-crusted-salmon-asparagus-tarragon.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Asparagus Salad with egg and Janbom de Bayonne",
+         "area": "Europe",
+         "image":"images/Asparagus Salad with egg and Janbom de Bayonne.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Lemony-roasted-salmon-with-white-wine-couscous",
+         "area": "Europe",
+         "image":"images/lemony-roasted-salmon-with-white-wine-couscous.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      },
+      {
+         "name":"Creamy avocado and White Bean Wrap",
+         "area": "Europe",
+         "image":"images/Creamy avocado and White Bean Wrap.jpg",
+         "ingredient":"A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+         "direction":"",
+         "comments": []
+      }
+]
+
